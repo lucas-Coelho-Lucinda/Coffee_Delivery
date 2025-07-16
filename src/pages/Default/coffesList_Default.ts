082c4c766df7c4e36febfa19ecdd62aac2d7895c
lucas-Coelho-Lucinda <Lucas.c.Lucinda@gmail.com>
2025-07-16 20:57:47 -0300

    fix: Rename files.

--- src/pages/Default/coffesList_Default.ts
+++ src/pages/Default/coffesList_Default.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
-export const coffesList = [
+export const coffesList_Default = [
   {
     id: uuidv4(),
     img: "/src/assets/Foto_itens/expresso_Tradicional.png",
