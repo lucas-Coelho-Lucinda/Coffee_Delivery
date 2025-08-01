40fc184a57c0ab6eb36350dc073b74b948ba3ce6
lucas-Coelho-Lucinda <Lucas.c.Lucinda@gmail.com>
2025-08-01 03:01:39 -0300

    fix:  CoffeeListToSell adjustment

--- src/pages/Default/components/CoffeeListToSell/index.tsx
+++ src/pages/Default/components/CoffeeListToSell/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { propsCoffeCard } from "../../types";
+
 import { ShoppingCart } from "@phosphor-icons/react";
 
 import {
@@ -22,68 +22,70 @@ import {
   TextDescriptionOfDevaultValue,
 } from "../../sytle";
 
-export const CoffeeListToSell = React.memo(
-  ({ coffe, onAdd, onChangeAmount }: propsCoffeCard) => {
-    return (
-      <OptionOfMenu key={coffe.id} optionSelected={coffe.is_selected}>
-        <CardImageOfCoffeToSell>
-          <img src={coffe.img} alt="" />
-        </CardImageOfCoffeToSell>
-        <TitleCoffeeContainer>
-          {coffe.coffeeCharacteristics.map((Feature) => (
-            <TypeCoffeeOffer key={Feature.id}>
-              {Feature.adjective}
-            </TypeCoffeeOffer>
-          ))}
-        </TitleCoffeeContainer>
+export const CoffeeListToSell = ({
+  coffe,
+  onAdd,
+  onChangeAmount,
+}: propsCoffeCard) => {
+  return (
+    <OptionOfMenu key={coffe.id} optionSelected={coffe.is_selected}>
+      <CardImageOfCoffeToSell>
+        <img src={coffe.img} alt="" />
+      </CardImageOfCoffeToSell>
+      <TitleCoffeeContainer>
+        {coffe.coffeeCharacteristics.map((Feature) => (
+          <TypeCoffeeOffer key={Feature.id}>
+            {Feature.adjective}
+          </TypeCoffeeOffer>
+        ))}
+      </TitleCoffeeContainer>
 
-        <TitleCoffee>{coffe.title}</TitleCoffee>
+      <TitleCoffee>{coffe.title}</TitleCoffee>
 
-        <SubTitleCoffee>{coffe.subTitles}</SubTitleCoffee>
-        <CardOfDeffaultValue>
-          <PriceContainerOnCoffee color="yellow-300" background="yellow-100">
-            <TextDescriptionOfDevaultValue>
-              Apartir de:
-            </TextDescriptionOfDevaultValue>
-            {coffe.valueDefault}
-          </PriceContainerOnCoffee>
+      <SubTitleCoffee>{coffe.subTitles}</SubTitleCoffee>
+      <CardOfDeffaultValue>
+        <PriceContainerOnCoffee color="yellow-300" background="yellow-100">
+          <TextDescriptionOfDevaultValue>
+            Apartir de:
+          </TextDescriptionOfDevaultValue>
+          {coffe.valueDefault}
+        </PriceContainerOnCoffee>
 
-          <PriceContainerOnCoffee color="yellow-300" background="yellow-100">
-            <TextDescriptionOfDevaultValue>
-              Frete fixo:
-            </TextDescriptionOfDevaultValue>
-            {coffe.deliveryValueDefault}
-          </PriceContainerOnCoffee>
+        <PriceContainerOnCoffee color="yellow-300" background="yellow-100">
+          <TextDescriptionOfDevaultValue>
+            Frete fixo:
+          </TextDescriptionOfDevaultValue>
+          {coffe.deliveryValueDefault}
+        </PriceContainerOnCoffee>
 
-          <PriceContainerOnCoffee color="purple-300" background="purple-100">
-            <TextDescriptionOfDevaultValue>
-              Frete atual:
-            </TextDescriptionOfDevaultValue>
-            {coffe.deliveryValue}
-          </PriceContainerOnCoffee>
-        </CardOfDeffaultValue>
-        <CardTransactionsCoffee>
-          <ValueOfCoffeCurrent>{coffe.value}</ValueOfCoffeCurrent>
-          <OrderButtonPositioningCard>
-            <CarButtonAddOrRemoveAmount>
-              <ButtonAddAmountNegative
-                onClick={() => onChangeAmount(coffe?.id, false)}
-              >
-                -
-              </ButtonAddAmountNegative>
-              <TextAddAmount>{coffe.amount}</TextAddAmount>
-              <ButtonAddAmountPlus
-                onClick={() => onChangeAmount(coffe?.id, true)}
-              >
-                +
-              </ButtonAddAmountPlus>
-            </CarButtonAddOrRemoveAmount>
-            <ButtonAddShopping onClick={() => onAdd(coffe?.id)}>
-              <ShoppingCart size={24} weight="fill" />
-            </ButtonAddShopping>
-          </OrderButtonPositioningCard>
-        </CardTransactionsCoffee>
-      </OptionOfMenu>
-    );
-  }
-);
+        <PriceContainerOnCoffee color="purple-300" background="purple-100">
+          <TextDescriptionOfDevaultValue>
+            Frete atual:
+          </TextDescriptionOfDevaultValue>
+          {coffe.deliveryValue}
+        </PriceContainerOnCoffee>
+      </CardOfDeffaultValue>
+      <CardTransactionsCoffee>
+        <ValueOfCoffeCurrent>{coffe.value}</ValueOfCoffeCurrent>
+        <OrderButtonPositioningCard>
+          <CarButtonAddOrRemoveAmount>
+            <ButtonAddAmountNegative
+              onClick={() => onChangeAmount(coffe?.id, false)}
+            >
+              -
+            </ButtonAddAmountNegative>
+            <TextAddAmount>{coffe.amount}</TextAddAmount>
+            <ButtonAddAmountPlus
+              onClick={() => onChangeAmount(coffe?.id, true)}
+            >
+              +
+            </ButtonAddAmountPlus>
+          </CarButtonAddOrRemoveAmount>
+          <ButtonAddShopping onClick={() => onAdd(coffe?.id)}>
+            <ShoppingCart size={24} weight="fill" />
+          </ButtonAddShopping>
+        </OrderButtonPositioningCard>
+      </CardTransactionsCoffee>
+    </OptionOfMenu>
+  );
+};
