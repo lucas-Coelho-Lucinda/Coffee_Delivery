f92dfd69054df3aa1401bbc9b634cacc596af2e9
lucas-Coelho-Lucinda <Lucas.c.Lucinda@gmail.com>
2025-08-01 02:56:30 -0300

    fix: Fixing router and implementing logic to assign backups according to the page.

--- Router.tsx
+++ Router.tsx
@@ -1,16 +1,45 @@
-import { Routes, Route } from "react-router-dom";
+
 import Shopping from "./src/pages/Shopping";
 import Header from "./src/components/Header";
 import LagoutDefault from "./src/pages/Default";
+import { useCallback, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { RequestConcluded } from "./src/pages/RequestConcluded";
 
+import {
+  saveOrderFinished,
+  commitCoffeesAndReturnValues,
+  toObtainFinalRequestedValues,
+  toObtainOrderAndReturnValues,
+} from "./src/Backup";
+
 function Router() {
+  const location = useLocation();
+
+  const activedBackUpOfRoot = useCallback((root: string) => {
+    if (root === "/") {
+      const order = toObtainOrderAndReturnValues();
+      commitCoffeesAndReturnValues(order.CoffeesInTheCart, true);
+    }
+
+    if (root === "/Request") {
+      const requestfineshed = toObtainFinalRequestedValues();
+
+      saveOrderFinished(requestfineshed);
+    }
+  }, []);
+  useEffect(() => {
+    const { pathname } = location;
+
+    activedBackUpOfRoot(pathname);
+  }, [activedBackUpOfRoot, location]);
+
   return (
     <Routes>
       <Route path="/" element={<Header />}>
-        <Route path="/" element={<LagoutDefault />}/>
-        <Route path="/Shopping" element={<Shopping />}/>
-         <Route path="/Request" element={<RequestConcluded />}/>
+        <Route path="/" element={<LagoutDefault />} />
+        <Route path="/Shopping" element={<Shopping />} />
+        <Route path="/Request" element={<RequestConcluded />} />
       </Route>
     </Routes>
   );
